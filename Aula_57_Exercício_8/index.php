2dcb3efce947d91c2f6469053777608348d2bde7
vandersonbfs <vanderson_b@hotmail.com>
2025-02-19 12:09:23 -0300

    Aula_57_Exercício_8

---
+++ Aula_57_Exercício_8/index.php
@@ -0,0 +1,12 @@
+<?php
+    $velocidade = 100; //int
+    $marca = "Ferrari"; //String
+    $itens = ["Teto Solar","Motor 6.3","Freios de Biclicleta","Bancos de fusca"]; //Array
+
+    echo $velocidade;
+    echo "<br>";
+    echo $marca;
+    echo "<br>";
+    print_r($itens);
+
+?>
