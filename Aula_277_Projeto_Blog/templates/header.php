8156e855507394c12df5912d8c0c0f4d22b82d94
vandersonbfs <vanderson_b@hotmail.com>
2025-04-28 11:40:26 -0300

    Aula_280_CSS_do_cabecalho_e_rodape

--- Aula_277_Projeto_Blog/templates/header.php
+++ Aula_277_Projeto_Blog/templates/header.php
@@ -14,5 +14,15 @@
 </head>
 <body>
     <header>
-        Template cabeçalho
+        <a href="<?= $BASE_URL ?>" id="logo">
+            <img src="<?= $BASE_URL ?>/img/logo.svg" alt="">
+        </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Categoria</a></li>
+                <li><a href="#" class="nav-link">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>/contato.php" class="nav-link">Contato</a></li>
+            </ul>
+        </nav>
     </header>
