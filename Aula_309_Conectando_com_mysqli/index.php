462d57d951f51c50a45b90eab244af1a592c2ec3
vandersonbfs <vanderson_b@hotmail.com>
2025-05-07 09:20:08 -0300

    Aula_310_Erro_na_conexao

--- Aula_309_Conectando_com_mysqli/index.php
+++ Aula_309_Conectando_com_mysqli/index.php
@@ -5,4 +5,9 @@
     $db = "cursophp";
 
     $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_errno) {
+        echo "Erro na conexão! <br>";
+        echo "Erro: " . mysqli_connect_error();
+    }
 ?>
